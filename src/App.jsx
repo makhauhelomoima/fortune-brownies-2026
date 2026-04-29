d4228b1bb0392a9631027c46c434c5580142e5ab
makhauhelomoima <makhauhelomoima@gmail.com>
2026-04-29 15:18:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Dashboard from './pages/Dashboard'
-import ResetPassword from './pages/ResetPassword'
-import AuthCallback from './pages/AuthCallback'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <Home />
 }
 
 export default App
